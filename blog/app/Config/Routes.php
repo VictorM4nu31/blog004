abadb38cdef0eec181de0ca8d34549b2fdcd655a
VictorM4nu31 <angelesvictor690@gmail.com>
2025-07-16 08:42:13 -0600

    refactor: actualizar las rutas de autenticación y administración para utilizar la sintaxis de clase en lugar de cadenas

--- blog/app/Config/Routes.php
+++ blog/app/Config/Routes.php
@@ -1,29 +1,32 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\AuthController;
+use App\Controllers\Admin\PostAdminController;
+use App\Controllers\Admin\CategoryAdminController;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'AuthController::login');
+$routes->get('/', [AuthController::class, 'login']);
 
-$routes->get('login', 'AuthController::login');
-$routes->post('login', 'AuthController::attemptLogin');
-$routes->get('logout', 'AuthController::logout');
-$routes->get('register', 'AuthController::register');
-$routes->post('register', 'AuthController::attemptRegister');
+$routes->get('login', [AuthController::class, 'login']);
+$routes->post('login', [AuthController::class, 'attemptLogin']);
+$routes->get('logout', [AuthController::class, 'logout']);
+$routes->get('register', [AuthController::class, 'register']);
+$routes->post('register', [AuthController::class, 'attemptRegister']);
 
 $routes->group('admin', ['filter' => 'auth'], function($routes) {
-    $routes->get('posts', 'Admin\PostAdminController::index');
-    $routes->get('posts/create', 'Admin\PostAdminController::create');
-    $routes->post('posts/store', 'Admin\PostAdminController::store');
-    $routes->get('posts/edit/(:num)', 'Admin\PostAdminController::edit/$1');
-    $routes->post('posts/update/(:num)', 'Admin\PostAdminController::update/$1');
-    $routes->get('posts/delete/(:num)', 'Admin\PostAdminController::delete/$1');
-    $routes->get('categories', 'Admin\CategoryAdminController::index');
-    $routes->get('categories/create', 'Admin\CategoryAdminController::create');
-    $routes->post('categories/store', 'Admin\CategoryAdminController::store');
-    $routes->get('categories/edit/(:num)', 'Admin\CategoryAdminController::edit/$1');
-    $routes->post('categories/update/(:num)', 'Admin\CategoryAdminController::update/$1');
-    $routes->get('categories/delete/(:num)', 'Admin\CategoryAdminController::delete/$1');
+    $routes->get('posts', [PostAdminController::class, 'index']);
+    $routes->get('posts/create', [PostAdminController::class, 'create']);
+    $routes->post('posts/store', [PostAdminController::class, 'store']);
+    $routes->get('posts/edit/(:num)', [PostAdminController::class, 'edit/$1']);
+    $routes->post('posts/update/(:num)', [PostAdminController::class, 'update/$1']);
+    $routes->get('posts/delete/(:num)', [PostAdminController::class, 'delete/$1']);
+    $routes->get('categories', [CategoryAdminController::class, 'index']);
+    $routes->get('categories/create', [CategoryAdminController::class, 'create']);
+    $routes->post('categories/store', [CategoryAdminController::class, 'store']);
+    $routes->get('categories/edit/(:num)', [CategoryAdminController::class, 'edit/$1']);
+    $routes->post('categories/update/(:num)', [CategoryAdminController::class, 'update/$1']);
+    $routes->get('categories/delete/(:num)', [CategoryAdminController::class, 'delete/$1']);
 });
